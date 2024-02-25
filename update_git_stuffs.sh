4eb47562f7ea1ed4b3e87a91d8ac50300cf32e07
chkg2a <xshalltearvamps@gmail.com>
2024-02-25 14:48:28 +0530

    updated

--- update_git_stuffs.sh
+++ update_git_stuffs.sh
@@ -2,7 +2,7 @@
 
 obs_file_path="/storage/emulated/0/"
 
-git clone git@github.com:chkg2a/obsidian_ChK
-git pull git@github.com:chkg2a/obsidian_ChK
+git clone git@github.com:chkg2a/obsidian_ChK ~/obsidian_ChK
+cd obsidian_ChK && git pull git@github.com:chkg2a/obsidian_ChK
 
-$(cp obsidian_ChK $obs_file_path)
+$(cp -r ~/obsidian_ChK $obs_file_path)
